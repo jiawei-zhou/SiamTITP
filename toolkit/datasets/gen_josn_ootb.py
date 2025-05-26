28a782c074d3b5b5d9455ee34196ab7771fc3141
Java zhou <126318942+jiawei-zhou@users.noreply.github.com>
2025-05-26 18:50:56 +0800

    Update gen_josn_ootb.py

--- toolkit/datasets/gen_josn_ootb.py
+++ toolkit/datasets/gen_josn_ootb.py
@@ -3,8 +3,8 @@ import json
 import numpy as np
 import pandas as pd
 import random
-def random_choices_gt_file():
-    path ='/home/zhoujiawei/satellite_video_datasets/OOTB'
+def gen_json_file():
+    path ='Your_dataset_path/OOTB'    # need to modified path
     videos = sorted(next(os.walk(path))[1])
     js = {}
     for video in videos:
@@ -31,7 +31,7 @@ def random_choices_gt_file():
         for mask,attr in zip(video_attr_mask,attrs):
             if mask:
                 video_attr.append(attr)
-        # 保存json
+        # save json information
         js[video_name] = {}
         js[video_name]['video_dir'] = video_dir
         js[video_name]['init_rect'] = init_bbox
@@ -39,35 +39,5 @@ def random_choices_gt_file():
         js[video_name]['gt_rect'] = gt_bbox
         js[video_name]['attr'] = video_attr
 
-    json.dump(js, open('/home/zhoujiawei/satellite_video_datasets/OOTB/OOTB_new.json', 'w'), indent=4, sort_keys=True)
-# def check_gtfiles():
-#     path ='/home/zhoujiawei/satellite_video_datasets/VISO/SOT/sot'
-#     classes = sorted(next(os.walk(path))[1])
-#     js = {}
-#     for classe in classes:
-#         videos = sorted(os.listdir(os.path.join(path,classe)))
-#         for video in videos:
-#             video_name = classe + '_' + video
-#             video_dir = video
-#             attr = None
-#             gt_path = os.path.join(path,classe,video,'gt')
-#             init_rect_list = []
-#             gt_rect_list = []
-#             gt_files = os.listdir(gt_path)
-#             new_gt_path = os.path.join(path,classe,video,'new_gt')
-#             if not os.path.exists(new_gt_path):
-#                 os.makedirs(new_gt_path)
-#             for gt_file in gt_files:
-#                 gt_file_path = os.path.join(gt_path,gt_file)
-#                 new_gt_file_path = os.path.join(new_gt_path,gt_file)
-#                 file_data = ""
-#                 with open(gt_file_path,'r') as f:
-#                     for line in f:
-#                         first_obj = line.split(',')[0]
-#                         if first_obj == '':
-#                             line = line.replace(',','',1)
-#                         file_data += line
-#                 with open(new_gt_file_path,'w') as f:
-#                     f.write(file_data)
-# check_gtfiles()              
-random_choices_gt_file()
+    json.dump(js, open('Your_dataset_path/OOTB.json', 'w'), indent=4, sort_keys=True)         
+gen_json_file()
